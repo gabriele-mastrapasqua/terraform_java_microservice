376627a45926669b727d617a4c8fdae8d033a5c1
Gabriele Mastrapasqua <gabriele.mastrapasqua@gmail.com>
2024-03-23 08:36:54 +0100

    wip

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,15 @@ JAVA_JAR_NAME="rest-service-0.0.1-SNAPSHOT"
 BEANSTALK_APP_NAME="JavaMicroTerraformTestApp"
 BEANSTALK_ENV_NAME="JavaMicroTerraformTestEnv"
 BEANSTALK_APP_VERSION="v1"
+BEANSTALK_S3_DEPLOY_BUCKET="${BEANSTALK_ENV_NAME}.applicationversion.bucket"
+BEANSTALK_S3_DEPLOY_KEY="beanstalk/${JAVA_JAR_NAME}"
 
-#aws elasticbeanstalk create-application-version --application-name YourAppName --version-label v1 --source-bundle S3Bucket=your-bucket-name,S3Key=path/to/your/$JAVA_JAR_NAME
+echo "build java jar..."
+cd code
+./gradleW clean && ./gradleW build
+
+echo "deploy on s3 the new app version..."
+aws s3 cp code/build/libs/$JAVA_JAR_NAME s3://$BEANSTALK_S3_DEPLOY__BUCKET/$BEANSTALK_S3_DEPLOY_KEY
+
+echo "update beanstalk with the new app version..."
 aws elasticbeanstalk update-environment --application-name $BEANSTALK_APP_NAME --environment-name $BEANSTALK_ENV_NAME --version-label $BEANSTALK_APP_VERSION
