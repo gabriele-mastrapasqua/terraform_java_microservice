2e3b048158f74b25daf2e6cb08eb6bc90d7cea5f
Gabriele Mastrapasqua <gabriele.mastrapasqua@gmail.com>
2024-03-24 09:54:05 +0100

    feat - add  a sample waf rule to limit unique ips per max 10k per day

--- terraform/waf.tf
+++ terraform/waf.tf
@@ -1,3 +1,13 @@
+# Define the rate-based rule name
+variable "rate_based_rule_name" {
+  default = "BlockAfter10kRequestsPerDay"
+}
+
+# Define the maximum request limit (10,000 requests per day)
+variable "max_requests_per_day" {
+  default = 10000
+}
+
 resource "aws_wafv2_web_acl" "my_web_acl" {
   name        = "my-web-acl"
   description = "Web ACL for my application"
@@ -10,11 +20,31 @@ resource "aws_wafv2_web_acl" "my_web_acl" {
   
   // Define your WAF rules and conditions here
 
+# Define the rate-based rule
+  rule {
+    name     = var.rate_based_rule_name
+    priority = 1
+    action {
+      block {}
+    }
+    statement {
+      rate_based_statement {
+        limit = var.max_requests_per_day
+        aggregate_key_type = "IP"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "BlockedRequests"
+      sampled_requests_enabled   = true
+    }
+  }
+
 
    // visibility config
    visibility_config {
       cloudwatch_metrics_enabled = false
-      metric_name                = "rule-1"
+      metric_name                = "waf-acl"
       sampled_requests_enabled   = false
     }
 
@@ -24,7 +54,8 @@ resource "aws_wafv2_web_acl" "my_web_acl" {
 
 }
 
-#resource "aws_wafv2_web_acl_association" "my_acl_association" {
-#  resource_arn = aws_elastic_beanstalk_environment.beanstalk_env.load_balancers[0].arn
-#  web_acl_arn  = aws_wafv2_web_acl.my_web_acl.arn
-#}
+# associate WAF with ALB balancer created from beanstalk
+resource "aws_wafv2_web_acl_association" "my_acl_association" {
+  resource_arn = aws_elastic_beanstalk_environment.beanstalk_env.load_balancers[0]
+  web_acl_arn  = aws_wafv2_web_acl.my_web_acl.arn
+}
